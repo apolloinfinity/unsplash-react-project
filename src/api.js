42bf921bd7a340cb054eb8a776bfaf48654025d9
Javier Ramirez Roa <apollo.infinity88@gmail.com>
2023-04-16 23:55:06 -0700

    WIP: Created en for api key.

--- src/api.js
+++ src/api.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
+const unsplashApiKey = process.env.REACT_APP_UNSPLASH_KEY;
+
 const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
-      Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`,
+      Authorization: `Client-ID ${unsplashApiKey}`,
     },
     params: {
       query: term,
     },
   });
-
   return response.data?.results;
 };
 
